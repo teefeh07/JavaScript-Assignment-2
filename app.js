75cb0815fa9c022f15bb57a357dba340fff2c2ef
teefeh07 <65070485+teefeh07@users.noreply.github.com>
2022-06-23 18:32:12 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+    let firstName= "Testimony";
+    let middleName= "Boluwatife";
+    let lastName= "Ayesa";
+    let height= "6'3ft";
+    let country= "Nigeria";
+
+    
+    const fullName= firstName + ' ' + middleName + ' ' + lastName;
+ 
+    document.write('Name:' + ' '+ fullName + '<br/>');
+    document.write('Height:' + ' '+ height + '<br/>');
+    document.write('Country:' + ' '+ country + '<br/>');
